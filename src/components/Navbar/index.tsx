860d1f4ced3457ba16fdee0fd36bd89f7aedb69a
Lithium Engineer <62958090+LithiumEngineer@users.noreply.github.com>
2023-07-18 00:03:57 -0600

    Added basic tabs to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,19 @@
 const Navbar = () => {
-  return <div>navbar</div>
+  return (
+    <div className="flex justify-between items-center w-full h-[70px] bg-gradient-to-r from-indigo-700 to-purple-900">
+      <div className="ml-10 text-white">Kevin Kang</div>
+      <div className="flex ">
+        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
+          Projects
+        </div>
+        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
+          Experience
+        </div>
+        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
+          Resume
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Navbar
