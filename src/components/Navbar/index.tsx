ce55e195d20c9a0bc549f20cc9c94927afddc92b
Lithium Engineer <62958090+LithiumEngineer@users.noreply.github.com>
2023-07-18 18:32:27 -0600

    changed navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,16 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center w-full h-[70px] bg-gradient-to-r from-indigo-700 to-purple-900">
+    <div className="flex justify-between items-center w-screen h-[100px] bg-gradient-to-r from-indigo-700 to-purple-900">
       <div className="ml-10 text-white">Kevin Kang</div>
       <div className="flex ">
-        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
-          Projects
+        <div className="px-5 mx-5 text-md text-white rounded-lg py-2 opacity-70 bg-opacity-0 hover:cursor-pointer bg-violet-950 hover:opacity-100 hover:bg-opacity-100 duration-700">
+          PROJECTS
         </div>
-        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
-          Experience
+        <div className="px-5 mx-5 text-md text-white rounded-lg py-2 opacity-70 bg-opacity-0 hover:cursor-pointer bg-violet-950 hover:opacity-100 hover:bg-opacity-100 duration-700">
+          EXPERIENCE
         </div>
-        <div className="px-5 mx-5 text-white opacity-70 hover:cursor-pointer hover:opacity-100 duration-500">
-          Resume
+        <div className="px-5 mx-5 text-md text-white rounded-lg py-2 opacity-70 bg-opacity-0 hover:cursor-pointer bg-violet-950 hover:opacity-100 hover:bg-opacity-100 duration-700">
+          RESUME
         </div>
       </div>
     </div>
