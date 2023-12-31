68ab5520559490eb023f0650bb971393e10cbd6f
LithiumEngineer <haochenkang@gmail.com>
2023-12-31 15:55:40 -0700

    updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,17 +4,13 @@ import { FOOTER_WORDS, FOOTER_WORDS_COLOR } from "@/constants"
 
 const Footer = () => {
   return (
-    <div className="flex justify-around mx-auto w-auto max-w-[1200px] p-10">
-      <div className="whitespace-nowrap flex-shrink flex items-center text-[#cdd3f7] h-fit min-w-fit">
-        <BiCopyright className="mr-2 w-5 h-5" />
-        <div className="w-full">Kevin Kang 2023</div>
+    <div className="w-screen pb-14">
+      <div className="text-center mx-auto">
+        <TypewriterEffect words={FOOTER_WORDS} colors={FOOTER_WORDS_COLOR} cursor={false} />
       </div>
-      <div className="flex-shrink text-center min-w-fit">
-        iaosdhfoiahdsfoiahdsiofhoiadshfoih
-        {/* <TypewriterEffect words={FOOTER_WORDS} colors={FOOTER_WORDS_COLOR} cursor={false} /> */}
-      </div>
-      <div className="flex-shrink whitespace-nowrap text-[#cdd3f7] min-w-fit">
-        All Rights Reserved.
+      <div className="flex text-[#cdd3f7] h-fit w-fit mx-auto mt-5">
+        <BiCopyright className="mr-2 my-auto w-3 h-3" />
+        <div className="w-fit text-xs">Kevin Kang 2023. All Rights Reserved.</div>
       </div>
     </div>
   )
