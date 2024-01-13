2f0dec50d437b076191a860d07fd187a85519f18
LithiumEngineer <haochenkang@gmail.com>
2024-01-13 16:31:38 -0500

    fixed dc model

--- src/components/Education/Blender.jsx
+++ src/components/Education/Blender.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/DC2.glb')
+  const { nodes, materials } = useGLTF('/test4.glb')
   return (
     <group {...props} dispose={null}>
       <group position={[1.513, -1.271, -4.063]}>
@@ -10,10 +10,10 @@ export function Model(props) {
         <mesh geometry={nodes.Cube001_1.geometry} material={materials.Material_1} position={[1.699, 1.721, 7.335]} />
         <mesh geometry={nodes.Cube002.geometry} material={materials.Material_2} position={[1.728, 1.723, 1.738]} />
       </group>
-      <mesh geometry={nodes.Text.geometry} material={materials.Material_1} position={[2.656, 1.91, 2.929]} rotation={[1.577, 0.149, -1.545]} />
-      <mesh geometry={nodes.Text001.geometry} material={materials['Material_1.001']} position={[-2.679, 1.954, -1.827]} rotation={[1.568, -0.132, 1.498]} />
+      <mesh geometry={nodes.Text.geometry} material={materials['Material.001']} position={[2.673, 1.944, 2.789]} rotation={[1.577, 0.149, -1.545]} />
+      <mesh geometry={nodes.Text001.geometry} material={materials['Material_1.001']} position={[-2.587, 1.964, -1.829]} rotation={[1.571, -0.13, 1.565]} />
     </group>
   )
 }
 
-useGLTF.preload('/DC2.glb')
+useGLTF.preload('/test4.glb')
